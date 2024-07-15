383d3cfa1d09da7601258c95ffc8476e56eee8d3
Mojtaba Movahedian <44787178+MMojtaba@users.noreply.github.com>
2024-07-15 12:21:16 -0700

    changed theme to light

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -16,7 +16,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>
+      <body className={inter.className} data-theme="light">
         <Navbar></Navbar>
         {children}
       </body>
