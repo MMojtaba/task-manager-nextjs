350cfe8eab982d6d191d50d190ab45af8a60fffe
Mojtaba Movahedian <44787178+MMojtaba@users.noreply.github.com>
2024-07-15 19:15:11 -0700

    setup middleware correctly

--- src/middleware.ts
+++ src/middleware.ts
@@ -3,29 +3,18 @@ import { auth, BASE_PATH } from "./app/auth/auth";
 // import dbConnect from "./app/utils/dbConnect";
 
 export const config = {
-  matcher: ["/tasks"],
+  matcher: ["/((?!_next/static|_next/image|favicon.ico).*)"],
 };
 
 export default auth(async (req) => {
-  console.log("in middleware", req.auth);
-  // return;
+  console.log("in middleware");
   // await dbConnect();
   const reqUrl = new URL(req.url);
-  if (!req.auth && reqUrl?.pathname !== "/") {
+  if (!req.auth && !["/login", "/register"].includes(reqUrl?.pathname)) {
     console.log("not auth");
-    return NextResponse.redirect(
-      // new URL(
-      //   `${BASE_PATH}/signIn`,
-      //   req.url
-      // )
-      new URL(
-        `${BASE_PATH}/signin?callbackUrl=${encodeURIComponent(
-          reqUrl?.pathname
-        )}`,
-        req.url
-      )
-    );
-  } else {
+    return NextResponse.redirect(new URL("login", req.url));
+  } else if (req.auth && ["/login", "/register"].includes(reqUrl?.pathname)) {
     console.log("is auth");
+    return NextResponse.redirect(new URL("/", req.url));
   }
 });
