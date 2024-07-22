f43e285c8da5877657486cb661c284ac28b9ff35
MMojtaba <44787178+MMojtaba@users.noreply.github.com>
2024-07-21 19:45:47 -0700

    added ability to view image large in carousel

--- src/app/images/responsive/myCarousel.tsx
+++ src/app/images/responsive/myCarousel.tsx
@@ -2,6 +2,7 @@
 
 import "react-responsive-carousel/lib/styles/carousel.min.css";
 import { Carousel } from "react-responsive-carousel";
+import { useState } from "react";
 
 interface Props {
   images: string[];
@@ -9,31 +10,56 @@ interface Props {
 }
 
 export default function MyCarousel({ images, single = false }: Props) {
+  const [openedImage, setOpenedImage] = useState<number>(0);
+
+  function handleImageClick(index: number) {
+    console.log("got clicked", index);
+    setOpenedImage(index);
+    document.getElementById("image_view_modal").showModal();
+  }
   return (
-    <Carousel
-      className=""
-      autoPlay={false}
-      centerMode={!single}
-      centerSlidePercentage={80}
-      axis="horizontal"
-      showStatus={true}
-      showIndicators={true}
-      showThumbs={single}
-      swipeable={false}
-      width={"100%"}
-      infiniteLoop={true}
-    >
-      {images.map((image, index) => {
-        return (
-          //   <div key={index} className="tmpDiv2">
+    <>
+      <Carousel
+        className=""
+        autoPlay={false}
+        centerMode={!single}
+        centerSlidePercentage={80}
+        axis="horizontal"
+        showStatus={true}
+        showIndicators={true}
+        showThumbs={single}
+        swipeable={false}
+        width={"100%"}
+        infiniteLoop={true}
+      >
+        {images.map((image, index) => {
+          return (
+            <div
+              key={index}
+              className=""
+              onClick={() => handleImageClick(index)}
+            >
+              <img
+                key={index}
+                src={image}
+                className={single ? "object-contain" : "object-cover"}
+              />
+            </div>
+          );
+        })}
+      </Carousel>
+
+      <dialog id="image_view_modal" className="modal">
+        <div className="max-h-5xl modal-box h-full max-w-5xl">
           <img
-            key={index}
-            src={image}
-            className={single ? "object-contain" : "object-cover"}
+            className="max-h-[80vh] w-full object-contain"
+            src={images[openedImage]}
           />
-          //   </div>
-        );
-      })}
-    </Carousel>
+        </div>
+        <form method="dialog" className="modal-backdrop">
+          <button>close</button>
+        </form>
+      </dialog>
+    </>
   );
 }
