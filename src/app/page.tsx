4f03363d5e26388d4fc7ca43e82c3233314ec302
MMojtaba <44787178+MMojtaba@users.noreply.github.com>
2024-08-23 09:12:20 -0700

    added part of task home

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,31 @@
 import { auth } from "@/app/auth/auth";
 import AuthButton from "./_components/AuthButton.server";
+import { ITask, TASK_STATUS } from "./models/Task";
+import { getUserLabels } from "./dataAccess/label";
+import { getMyTasks } from "./dataAccess/task";
+import TaskRow from "./(routes)/tasks/components/TaskRow";
 
 export default async function Home() {
   const session = await auth();
 
+  let labels: string[] = [];
+  let tasks: ITask[] = [];
+
+  try {
+    // TODO: filter by label
+    const resTasks = await getMyTasks({ status: TASK_STATUS.IN_PROGRESS });
+    if (resTasks.status === 200) tasks = resTasks.data;
+
+    const resLabels = await getUserLabels();
+  } catch (err) {
+    console.error("Error getting home page data");
+  }
+
   return (
     <main>
-      <h1>Home page!</h1>
-      <pre>{JSON.stringify(session, null, 2)}</pre>
-      <AuthButton />
+      {tasks.map((task) => (
+        <TaskRow key={task._id} task={task} />
+      ))}
     </main>
   );
 }
