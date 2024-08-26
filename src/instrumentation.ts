ae0ce977df1c4e34016d8df11a8a653beeb7f854
MMojtaba <44787178+MMojtaba@users.noreply.github.com>
2024-08-26 11:57:35 -0700

    cleanup

--- src/instrumentation.ts
+++ src/instrumentation.ts
@@ -1,5 +1,4 @@
 export async function register() {
-  console.log("inside instru");
   if (process.env.NEXT_RUNTIME === "nodejs") {
     const dbConnect = require("./app/utils/dbConnect");
     await dbConnect.default();
